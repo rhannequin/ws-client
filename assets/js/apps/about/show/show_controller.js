fa28864adc38bbe1c86dbf2fb223d054ff6ee952
Rémy Hannequin <remy.hannequin@gmail.com>
2014-05-29 18:13:40 +0100

    Remove previous app to make a new one from scratch

--- assets/js/apps/about/show/show_controller.js
+++
@@ -1,8 +0,0 @@
-PlaceManager.module("AboutApp.Show", function(Show, PlaceManager, Backbone, Marionette, $, _){
-  Show.Controller = {
-    showAbout: function(){
-      var view = new Show.Message();
-      PlaceManager.mainRegion.show(view);
-    }
-  };
-});
