c59fb67454c81a944ec58615eab7837867794414
Rémy Hannequin <remy.hannequin@gmail.com>
2014-05-29 18:59:43 +0100

    Begin the new app with Bootstrap, jQuery and an ajax call test

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 ws-client
 =========
+
+## Install
+
+```
+git clone git@github.com:rhannequin/ws-client.git
+cd ws-client
+```
+
+## Launch
+
+If you have Python installed, you can launch a webserver from this singlepage webapp:
+
+```
+python -c 'import SimpleHTTPServer; SimpleHTTPServer.test()'
+```
+
+Otherwise you can open the file `index.html` in your browser.
+
+You may want to make this webapp work (*duh!*), by launching it's remote API webservice [ws-app](https://github.com/rhannequin/ws-app).
---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+(function(win, doc){
+  'use strict';
+
+  var WS = win.WS = win.WS || {}
+
+  WS.version = 0.1
+  WS.apiUrl = 'http://localhost:5000'
+
+  WS.apiRequest = function(endpoint, method, data) {
+    endpoint = endpoint || '/'
+    method = method || 'GET'
+    data = data || {}
+    return $.ajax({
+        url: WS.apiUrl + endpoint
+      , type: method
+      , data: data
+      , dataType: 'json'
+    })
+  }
+
+  WS.apiRequest().done(function(result) {
+    console.log(result.data)
+  })
+
+})(window, window.document)
